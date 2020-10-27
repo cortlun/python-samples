3c8599a3dcd5d43a6b815c34db9ed173e161d734
Cort Lunke <CMLunke@landolakes.com>
2020-10-27 14:00:35 -0500

    Init

---
+++ material-dates.py
@@ -0,0 +1,18 @@
+import uuid
+import pandas as pd
+from datetime import datetime, timedelta
+
+
+material_ids = [str(uuid.uuid1()) for i in range(0,5)] # Generate a list of material ids
+
+
+start_date = datetime.strptime("2020-10-12", "%Y-%m-%d") # Specify start date
+end_date = datetime.strptime("2020-10-15", "%Y-%m-%d") # Specify end date
+dates_list = [start_date + timedelta(days=i) for i in range(0, (end_date - start_date).days + 1)] # Generate a list of dates, inclusive
+
+output = [{
+    "date": date.strftime("%Y-%m-%d"),
+    "uuid": material_id
+} for material_id in material_ids for date in dates_list] # Use nested list comprehension to produce output as a list of dictionaries
+
+output_df = pd.DataFrame(output) # Generate a DataFrame with the output you want
